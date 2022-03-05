14a9d29b90c3f3665e18bf6b3777c4bff8854c2e
Juan Jose <jjcg98@gmail.com>
2022-03-05 10:54:17 -0500

    Terminado logica del encriptador

--- js.js
+++ js.js
@@ -1,4 +1,47 @@
+// captura de lo que esta en el campto del textarea
+
+mensaje = document.getElementById("dseencriptar");
+
+
+//captura del evento click en boton encriptar
+var botonEncriptar = document.getElementById("btn-encriptar");
+botonEncriptar.onclick = encriptar;
+
+//captura del evento click en boton desencriptar
+var botonDesencriptar = document.getElementById("btn-desencriptar");
+botonDesencriptar.onclick = desencriptar;
+
+
+//Funcion encriptar
 function encriptar(){
-    var encriptar = document.getElementById("encriptar");
-       
+    var texto = document.getElementById("encriptar").value;
+
+    var nuevotextoe = texto.replace(/e/igm, "enter")// utilizo expresiones regulares --> /x/igm 
+    var nuevotextoi = nuevotextoe.replace(/i/igm, "imes"); 
+    var nuevotextoa = nuevotextoi.replace(/a/igm, "ai");
+    var nuevotextoo = nuevotextoa.replace(/o/igm, "ober");
+    var nuevotextou = nuevotextoo.replace(/u/igm, "ufat");
+    
+    document.getElementById("desencriptar").value = nuevotextou;
+    document.getElementById("encriptar").value = "";
+    
+    
+    
 }
+
+
+//Funcion desencriptar
+function desencriptar(){
+    var texto = document.getElementById("encriptar").value;
+
+    nuevotextoe = texto.replace(/enter/igm, "e");// utilizo expresiones regulares --> /x/igm 
+    nuevotextoi = nuevotextoe.replace(/imes/igm, "i"); 
+    nuevotextoa = nuevotextoi.replace(/ai/igm, "a");
+    nuevotextoo = nuevotextoa.replace(/ober/igm, "o")
+    nuevotextou = nuevotextoo.replace(/ufat/igm, "u")
+
+    document.getElementById("desencriptar").value = nuevotextou;
+    document.getElementById("encriptar").value = "";
+    
+}
+
